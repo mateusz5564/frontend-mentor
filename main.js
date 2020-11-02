0589472406da7a65346f06e4d8fd188cd4a5fc69
mateusz5564 <mateusz5564@gmail.com>
2020-11-02 21:42:16 +0100

    improve accessibility

--- main.js
+++ main.js
@@ -1,8 +1,38 @@
 const questionBtns = document.querySelectorAll(".faq__question-btn");
+questionBtns.forEach(btn => btn.addEventListener('click', handleButton));
 
-questionBtns.forEach(btn => btn.addEventListener('click', toggleAnswer));
+function handleButton(e) {
+  const question = this.parentElement.parentElement;
+  const questionArrow = question.querySelector('.faq__question-arrow');
+  const answer = question.querySelector('.faq__answer');
 
-function toggleAnswer(e) {
-  this.parentElement.querySelector('.faq__answer').classList.toggle('faq__answer--active')
+  closeAllAnswers();
+
+  //question state
   this.classList.toggle("faq__question-btn--active");
+  questionArrow.classList.toggle('faq__question-arrow--active');
+  this.getAttribute('aria-expanded') == 'true' ? this.setAttribute('aria-expanded', 'false') : this.setAttribute('aria-expanded', 'true');
+
+  //answer state
+  toggleAnswer(answer);
+}
+
+function toggleAnswer(answer) {
+  answer.classList.toggle('faq__answer--active');
+  answer.getAttribute('aria-hidden') == 'true' ? answer.setAttribute('aria-hidden', 'false') : answer.setAttribute('aria-hidden', 'true');
+}
+
+function closeAllAnswers() {
+  const questions = document.querySelectorAll('.faq__question');
+
+  questions.forEach(question => {
+    //question
+    question.querySelector('.faq__question-btn').classList.remove("faq__question-btn--active");
+    question.querySelector('.faq__question-arrow').classList.remove('faq__question-arrow--active');
+    question.querySelector('.faq__question-btn').setAttribute('aria-expanded', 'false');
+
+    //answer
+    question.querySelector('.faq__answer').classList.remove('faq__answer--active');
+    question.querySelector('.faq__answer').setAttribute('aria-hidden', 'true');
+  })
 }
