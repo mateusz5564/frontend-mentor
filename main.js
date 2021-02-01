3356aec4d756abca9ef23abe4f8db860ec212128
mateusz5564 <mateusz5564@gmail.com>
2021-02-01 21:06:41 +0100

    fix mobile menu styling and prevent scrolling when it's opened

--- main.js
+++ main.js
@@ -2,14 +2,16 @@ const menuBtn = document.querySelector('.header__btn');
 const menuBtnIcon = document.querySelector('.header__btn-icon');
 const menu = document.querySelector('.mobile-nav')
 
-const handleMenu = (e) => {
+const openAndCloseMenu = (e) => {
   if (menu.style.transform) {
     menu.style.transform = "";
+    document.body.classList.remove('menu-open');
     menuBtnIcon.setAttribute('src', './images/icon-hamburger.svg');
   } else {
     menu.style.transform = "translateY(0)";
+    document.body.classList.add('menu-open');
     menuBtnIcon.setAttribute('src', './images/icon-close.svg');
   }
 }
 
-menuBtn.addEventListener("click", handleMenu);
+menuBtn.addEventListener("click", openAndCloseMenu);
