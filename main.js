b24a1964bea52115e19441d63869a5deb7cba958
mateusz5564 <mateusz5564@gmail.com>
2021-02-01 21:59:05 +0100

    improve accessibility

--- main.js
+++ main.js
@@ -5,10 +5,12 @@ const menu = document.querySelector('.mobile-nav')
 const openAndCloseMenu = (e) => {
   if (menu.style.transform) {
     menu.style.transform = "";
+    menu.style.display = "none";
     document.body.classList.remove('menu-open');
     menuBtnIcon.setAttribute('src', './images/icon-hamburger.svg');
   } else {
     menu.style.transform = "translateY(0)";
+    menu.style.display = "block";
     document.body.classList.add('menu-open');
     menuBtnIcon.setAttribute('src', './images/icon-close.svg');
   }
