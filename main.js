cdc8f3c7878db9cfd6c895cc5743d2dedd33be6a
mateusz5564 <mateusz5564@gmail.com>
2021-02-03 19:31:03 +0100

    refactor menu toggling

--- main.js
+++ main.js
@@ -1,19 +1,32 @@
-const menuBtn = document.querySelector('.header__btn');
-const menuBtnIcon = document.querySelector('.header__btn-icon');
-const menu = document.querySelector('.mobile-nav')
+const menuBtn = document.querySelector(".header__btn");
+const menuBtnIcon = document.querySelector(".header__btn-icon");
+const menu = document.querySelector(".mobile-nav");
+
 
 const openAndCloseMenu = (e) => {
-  if (menu.style.transform) {
-    menu.style.transform = "";
-    menu.style.display = "none";
-    document.body.classList.remove('menu-open');
-    menuBtnIcon.setAttribute('src', './images/icon-hamburger.svg');
+  console.log(document.activeElement)
+  if (isMenuOpen()) {
+    closeMenu();
   } else {
-    menu.style.transform = "translateY(0)";
-    menu.style.display = "block";
-    document.body.classList.add('menu-open');
-    menuBtnIcon.setAttribute('src', './images/icon-close.svg');
+    openMenu();
   }
-}
+};
+
+const isMenuOpen = () => menu.classList.contains("mobile-nav--open");
+
+const closeMenu = () => {
+  menu.classList.remove("mobile-nav--open");
+  
+  document.body.classList.remove("modal-open");
+  menuBtnIcon.setAttribute("src", "./images/icon-hamburger.svg");
+};
+
+const openMenu = () => {
+  
+  menu.classList.add("mobile-nav--open");
+  
+  document.body.classList.add("modal-open");
+  menuBtnIcon.setAttribute("src", "./images/icon-close.svg");
+};
 
 menuBtn.addEventListener("click", openAndCloseMenu);
