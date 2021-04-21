fae3559355a9cda1cf78a04def21d6cd2382f69a
mateusz5564 <mateusz5564@gmail.com>
2021-04-21 16:10:50 +0200

    init project

---
+++ todo-app/src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+  Todo
+
+  {/* Add dynamic number -->*/} items left
+
+  All
+  Active 
+  Completed
+
+  Clear Completed
+
+  Drag and drop to reorder list
+
+    </div>
+  );
+}
+
+export default App;
