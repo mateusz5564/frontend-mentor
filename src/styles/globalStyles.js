4c6193b48c02c18e8b41f370884475e34dbf94e0
mateusz5564 <mateusz5564@gmail.com>
2021-06-18 14:32:03 +0200

    move fonts imports to index

--- src/styles/globalStyles.js
+++ src/styles/globalStyles.js
@@ -1,9 +1,6 @@
 import { createGlobalStyle } from "styled-components";
 
 export default createGlobalStyle`
-@import url('https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@300;600;800&display=swap');
-@import url('https://fonts.googleapis.com/css2?family=Pattaya&display=swap');
-@import url('https://fonts.googleapis.com/css2?family=Girassol&display=swap');
 
 * {
   margin: 0;
