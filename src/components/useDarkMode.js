89dfce8d2c5e5f3a0bed00a14308557d9f269d0f
mateusz5564 <mateusz5564@gmail.com>
2021-06-18 14:35:46 +0200

    remove useEffect from useDarkMode

--- src/components/useDarkMode.js
+++ src/components/useDarkMode.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
 export const useDarkMode = () => {
   const [theme, setTheme] = useState(() => {
@@ -13,11 +13,6 @@ export const useDarkMode = () => {
   const themeToggler = () => {
     theme === "light" ? setMode("dark") : setMode("light");
   };
-
-  // useEffect(() => {
-  //   const localTheme = window.localStorage.getItem("theme");
-  //   localTheme && setTheme(localTheme);
-  // }, []);
 
   return [theme, themeToggler];
 };
