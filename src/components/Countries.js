154d892e3543fe4cf74bee35b41c492e95e2f962
mateusz5564 <mateusz5564@gmail.com>
2021-06-21 17:50:23 +0200

    add status and render CountryCard skeletons

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -3,17 +3,21 @@ import { Link } from "react-router-dom";
 import styled from "styled-components";
 import React from "react";
 import { useEffect, useState } from "react";
+import SkeletonCountryCard from "./SkeletonCountryCard";
 
 const Countries = () => {
   const [countries, setCountries] = useState([]);
+  const [status, setStatus] = useState("idle");
 
   useEffect(() => {
+    setStatus("fetching");
     fetch("https://restcountries.eu/rest/v2/all")
       .then(res => {
         return res.json();
       })
       .then(json => {
         setCountries(json);
+        setStatus("finished");
       });
   }, []);
 
@@ -27,7 +31,21 @@ const Countries = () => {
     });
   };
 
-  return <React.Fragment>{renderCountries()}</React.Fragment>;
+  const renderSkeletons = () => {
+    const ids = [0,1,2,3,4,5,6,7,8];
+
+    return ids.map(id => {
+      return <SkeletonCountryCard key={id} />
+    })
+  }
+
+  if (status === "finished") {
+    return <>{renderCountries()}</>;
+  } else if (status === "fetching") {
+    return <>{renderSkeletons()}</>;
+  } else {
+    return <></>;
+  }
 };
 
 const CardLink = styled(Link)`
