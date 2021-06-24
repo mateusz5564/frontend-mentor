a628e9263e49b2907621d3c78f4ce01e0ed9b361
mateusz5564 <mateusz5564@gmail.com>
2021-06-24 18:46:27 +0200

    add max-height and format SkeletonElement

--- src/components/SkeletonElement.js
+++ src/components/SkeletonElement.js
@@ -2,19 +2,20 @@ import styled from "styled-components";
 import Shimmer from "./Shimmer";
 
 const SkeletonElement = ({ className, width, height, margin }) => {
-  return ( 
+  return (
     <Element className={className} width={width} height={height} margin={margin}>
       <Shimmer />
     </Element>
-  )
+  );
 };
 
 const Element = styled.div`
   position: relative;
   overflow: hidden;
-  width: ${({width}) => width};
-  height: ${({height}) => height};
-  margin: ${({margin}) => margin};
+  width: ${({ width }) => width};
+  max-width: 100%;
+  height: ${({ height }) => height};
+  margin: ${({ margin }) => margin};
   background-color: ${({ theme }) => theme.colors.skeletonBackground};
   border-radius: 4px;
 `;
