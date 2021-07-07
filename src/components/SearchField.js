13dd48cbf7fa0e341594a1c79d4f2fa9e3f3a206
mateusz5564 <mateusz5564@gmail.com>
2021-07-07 15:37:16 +0200

    polish SearchField styles

--- src/components/SearchField.js
+++ src/components/SearchField.js
@@ -1,49 +1,53 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 import { IoSearchSharp } from "react-icons/io5";
 
-const SearchField = ({onChange}) => {
-
-  return ( 
-    <Wrapper>
+const SearchField = ({ onChange, className }) => {
+  return (
+    <Wrapper className={className}>
       <SearchIcon />
-      <Input type="text" placeholder="Search for a country..." onChange={(e) => onChange(e.target.value)}/>
+      <Input
+        type="text"
+        placeholder="Search for a country..."
+        onChange={e => onChange(e.target.value)}
+      />
     </Wrapper>
-   );
-}
- 
+  );
+};
+
 const Wrapper = styled.div`
-  display: inline-flex;
+  display: flex;
   align-items: center;
   height: 60px;
-  background-color: ${({theme}) => theme.colors.element };
-  margin-bottom: 20px;
+  background-color: ${({ theme }) => theme.colors.element};
   border-radius: 5px;
+  box-shadow: ${({ theme }) => theme.shadowElement};
 `;
 
 const SearchIcon = styled(IoSearchSharp)`
   font-size: 1.5em;
   margin-left: 20px;
   margin-right: 15px;
-  color: ${({theme}) => theme.colors.textPlaceholder };
+  color: ${({ theme }) => theme.colors.textPlaceholder};
 `;
 
 const Input = styled.input`
+  width: 100%;
   height: 100%;
   border: transparent;
   border-radius: 5px;
   font-size: 1em;
-  background-color: ${({theme}) => theme.colors.element };
-  color: ${({theme}) => theme.colors.text };
+  background-color: ${({ theme }) => theme.colors.element};
+  color: ${({ theme }) => theme.colors.text};
   padding-left: 5px;
   padding-right: 10px;
 
   :focus {
     outline: none;
-    box-shadow: 0 0 0 1px ${({theme}) => theme.colors.focusBorder};
+    box-shadow: 0 0 0 1px ${({ theme }) => theme.colors.focusBorder};
   }
 
   ::placeholder {
-    color: ${({theme}) => theme.colors.textPlaceholder };
+    color: ${({ theme }) => theme.colors.textPlaceholder};
   }
 `;
 
