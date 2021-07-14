1d2082b6cb8e25f1a27ef4cde65da8b72a256010
mateusz5564 <mateusz5564@gmail.com>
2021-07-14 21:09:19 +0200

    add some styles to BackLink

--- src/components/BackLink.js
+++ src/components/BackLink.js
@@ -6,7 +6,7 @@ const BackLink = () => {
   const history = useHistory();
 
   return (
-    <Link onClick={() => history.goBack()}>
+    <Link href="" onClick={() => history.goBack()}>
       <BsArrowLeft style={{ fontSize: "1.5em" }}></BsArrowLeft>
       <span>Back</span>
     </Link>
@@ -24,6 +24,12 @@ const Link = styled.a`
   box-shadow: 0 0 6px rgba(0, 0, 0, 0.2);
   border-radius: 4px;
   cursor: pointer;
+  color: ${({ theme }) => theme.colors.text};
+
+  :focus {
+    outline: none;
+    box-shadow: 0 0 0 1px ${({ theme }) => theme.colors.focusBorder};
+  }
 
   span {
     margin: 0 5px;
