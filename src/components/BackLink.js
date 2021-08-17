f70d8d0d14feeef01408265f2d4bc24217b3c825
mateusz5564 <mateusz5564@gmail.com>
2021-08-17 23:56:57 +0200

    format code and add some styles

--- src/components/BackLink.js
+++ src/components/BackLink.js
@@ -1,30 +1,31 @@
-import { useHistory } from "react-router-dom";
-import styled from "styled-components";
-import { BsArrowLeft } from "react-icons/bs";
+import { useHistory } from 'react-router-dom';
+import styled from 'styled-components';
+import { BsArrowLeft } from 'react-icons/bs';
 
 const BackLink = () => {
   const history = useHistory();
 
   return (
-    <Link href="" onClick={() => history.goBack()}>
-      <BsArrowLeft style={{ fontSize: "1.5em" }}></BsArrowLeft>
+    <GoBack href="" onClick={() => history.goBack()}>
+      <BsArrowLeft style={{ fontSize: '1.5em' }} />
       <span>Back</span>
-    </Link>
+    </GoBack>
   );
 };
 
 export default BackLink;
 
-const Link = styled.a`
+const GoBack = styled.button`
   display: inline-flex;
   align-items: center;
-  text-decoration: none;
   margin: 20px 0 60px;
   padding: 8px 20px;
   box-shadow: 0 0 6px rgba(0, 0, 0, 0.2);
+  border: transparent;
   border-radius: 4px;
   cursor: pointer;
   color: ${({ theme }) => theme.colors.text};
+  background-color: ${({ theme }) => theme.colors.element};
 
   :focus {
     outline: none;
